59dc6f70f5714d0c42da532f36f05188e9d25a3d
Shah1272 <Free.me7788@gmail.com>
2024-07-08 18:57:41 +0000

    Data types

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,22 @@
-console.log("Muhammad");
+const personID = 10
+let personName = "Muhammad"
+var personEmail = "M122@gmail.com"
+personAddress = "Khiali"
+//console.log(personID);
+//console.table([personID, personName, personEmail])
+
+/*
+Never use var becuase of sope problem in js 
+*/
+
+
+let value = "10a"
+console.log(typeof(value))
+let Num = Number(value)
+console.log(typeof(Num))
+console.log(Num)
+
+
+let int = 1;
+let boolInt = Boolean(int);
+console.log(typeof(boolInt))
