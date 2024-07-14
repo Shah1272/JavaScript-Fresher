ae7449a8c7f62778a0c76e416c0e85fb598d52e7
Shah1272 <Free.me7788@gmail.com>
2024-07-14 11:38:08 +0000

    chaicodeMonth challenges

---
+++ chaicodemonth/operatorsDay2.js
@@ -0,0 +1,22 @@
+console.log(`Arithmatic Operators`)
+console.log(`Task 1`)
+let a = 7
+let b = 4
+console.log(`Addition of Two numbers is ${a+b}`)
+console.log(`Task 2`)
+console.log(`subtract of Two numbers is ${a-b}`)
+console.log(`Task 3`)
+console.log(`multiplication of Two numbers is ${a*b}`)
+console.log(`Task 4`)
+console.log(`division of Two numbers is ${a/b}`)
+console.log(`Task 5`)
+console.log(`Remainder is ${a%b}`)
+console.log(`Assignment Operators`)
+console.log(`Task 6&7`)
+let c = 19
+let d = 21
+const add = c+d
+console.log(add)
+const sub = d-c
+console.log(sub)
+
